05b9081964810b836090314bcc14e50d61ae6eaf
Савва <jigula228@gmail.com>
2024-04-06 17:46:36 +0300

    Надо подкачаться

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,14 +1,15 @@
 import { isEscapeKey } from './util.js';
 import { setScale, defaultScale } from './edit-image.js';
+import { sendData, submitButtonText } from './get-api.js';
 
 const uploadForm = document.querySelector('.img-upload__form'); // форма отправки информации о фотографии на сервер
-const uploadInput = document.querySelector('.img-upload__input'); // поле для загрузки фотографии
-const uploadOverlay = document.querySelector('.img-upload__overlay'); // модальное окно редактирования фотографии
-const hashtagInput = document.querySelector('.text__hashtags');
-const commentInput = document.querySelector('.text__description');
+const uploadInput = uploadForm.querySelector('.img-upload__input'); // поле для загрузки фотографии
+const uploadOverlay = uploadForm.querySelector('.img-upload__overlay'); // модальное окно редактирования фотографии
+const hashtagInput = uploadForm.querySelector('.text__hashtags');
+const commentInput = uploadForm.querySelector('.text__description');
+const imgUploadPreview = uploadForm.querySelector('.img-upload__preview img'); // просмотр фото для загрузки
 const btnCloseUploadForm = uploadForm.querySelector('.img-upload__cancel'); // крестик закрытия на большом изображении
-const btnUploadSubmit = document.querySelector('.img-upload__submit'); // кнопка "Опубликовать"
-
+const btnUploadSubmit = uploadForm.querySelector('.img-upload__submit'); // кнопка "Опубликовать"
 const hashtagErrorMessages = {
   1: `Хэш-тег должен начинаться с символа #.<br>
     Хэш-тег не может состоять только из одного символа.<br>
@@ -38,7 +39,6 @@ const pristine = new Pristine(uploadForm, {
 });
 // массив для запоминания всех ошибок в процессе валидации
 let hashtagErrorList = [];
-
 // функция валидации хештегов, которая передаётся в библиотеку Pristine
 const validateHashtag = (value) => {
   const regex = /^#[a-zа-яё0-9]{1,19}$/i;
@@ -61,7 +61,6 @@ const validateHashtag = (value) => {
   if (hashtagsFiltered.length > 5) {
     hashtagErrorList.push(3);
   }
-
   // если в переменной hashtagErrorList есть данные, это означает, что в процессе валидации были найдены ошибки
   if (hashtagErrorList.length === 0) {
     const err = pristine.getErrors(commentInput);
@@ -74,7 +73,6 @@ const validateHashtag = (value) => {
     return false;
   }
 };
-
 // функция валидации комментариев, которая передаётся в библиотеку Pristine
 const validateComment = (value) => {
   if (value.length > 140) {
@@ -88,7 +86,6 @@ const validateComment = (value) => {
     return true;
   }
 };
-
 // возвращаем текст первой ошибки из массива
 const showErrorMessage = () => hashtagErrorMessages[hashtagErrorList[0]];
 pristine.addValidator(
@@ -96,25 +93,33 @@ pristine.addValidator(
   validateHashtag,
   showErrorMessage
 );
-
 pristine.addValidator(
   commentInput,
   validateComment,
   'Комментарий не может содержать более 140 символов.'
 );
-
+// функция подставляет загружаемое фото в форму предварительного просмотра
+const getPhotoPreview = (evt) => {
+  const file = evt.target.files[0]; // получаем первый файл, выбранный пользователем в input элементе
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      imgUploadPreview.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+};
 // функция открывает форму загрузки
 const openUploadForm = () => {
+  document.querySelector('#effect-none').checked = true; // выбираем radio button с оригинальным эффектом
+  setScale(defaultScale); // установка дефолтного масштаба изображения
+  setEffect(defaultEffect); // установка дефолтного эффекта изображения
   document.body.classList.add('modal-open');
   uploadOverlay.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentKeydown); // закрывает окно по нажатию клавиши Esc
 };
 // функция закрывает форму загрузки
 const closeUploadForm = () => {
-  document.querySelector('#effect-none').checked = true; // выбираем radio button с оригинальным эффектом
-  setScale(defaultScale); // установка дефолтного масштаба изображения
-  // eslint-disable-next-line no-undef
-  setEffect(defaultEffect); // установка дефолтного эффекта изображения
   uploadOverlay.classList.add('hidden');
   uploadInput.value = '';
   hashtagInput.value = '';
@@ -123,7 +128,8 @@ const closeUploadForm = () => {
   document.removeEventListener('keydown', onDocumentKeydown); // удаляем обработчик нажатия клавиши Esc
   pristine.reset(); // очищаем ошибки и состояние валидации
 };
-uploadInput.addEventListener('change', () => {
+uploadInput.addEventListener('change', (evt) => {
+  getPhotoPreview(evt);
   openUploadForm();
 });
 // закрывает форму загрузки по нажатию на крестик
@@ -131,3 +137,16 @@ btnCloseUploadForm.addEventListener('click', (evt) => {
   evt.preventDefault();
   closeUploadForm();
 });
+// обработчик отправки формы
+const onUploadFormSubmit = async (evt) => {
+  evt.preventDefault();
+  btnUploadSubmit.disabled = true;
+  btnUploadSubmit.textContent = submitButtonText.SENDING;
+  await sendData(evt.target);
+  closeUploadForm();
+};
+
+// установка обработчика отправки формы
+uploadForm.addEventListener('submit', onUploadFormSubmit);
+
+export { btnUploadSubmit };

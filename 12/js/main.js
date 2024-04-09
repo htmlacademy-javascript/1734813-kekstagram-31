bfdbc77728ab84bdf545b74d293e19594214f2d3
Keks <keks@htmlacademy.ru>
2024-04-09 16:51:32 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,4 @@
+import './drawing.js';
+import './validation-form.js';
+import './image-editing.js';
+import './image-filter.js'
---
+++ 12/js/util.js
@@ -0,0 +1,14 @@
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { getRandomNumber, getRandomElement, isEscapeKey, debounce };

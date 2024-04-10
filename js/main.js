e91c00c839403575a1efdac436b9475f77a60c3f
Савва <jigula228@gmail.com>
2024-04-10 21:01:36 +0300

    проблемы с мастером

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import { renderPicturesList } from './images-list';
 import { showDataErrorToast } from './pop-up-errors';
 import './post';
 import './upload-form';
-import * as filters from './image-filters';
+import * as filters from gig;
 
 fetchPicturesData()
   .then(() => {
